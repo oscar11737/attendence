eb492d98b407876a4053c5959f0777bb32f19e76
Oscar Chang <officially.oscar@gmail.com>
2020-05-21 22:20:34 +1200

    work on adding new family, linking family and date, make intermediate record

--- attendFellowship/views.py
+++ attendFellowship/views.py
@@ -13,8 +13,7 @@ sql_s5 = "on attendFellowship_family.id = attendFellowship_meetingdate_family.fa
 
 def home(request):
     max_date = MeetingDate.objects.latest('submission_date')
-    families = IntermediateRecord.objects.filter(submission_date=max_date.submission_date)
-    families_list = Family.objects.all()
+    families = Family.objects.all()
     print
     if request.method == 'POST':
         form = TodayDateForm(request.POST)
@@ -26,7 +25,6 @@ def home(request):
         context['max_date'] = max_date
         context['form'] = TodayDateForm()
         context['families'] = families
-        context['families_list'] = families_list
     return render(request, 'home.html', context)
 
 def new_family(request):
@@ -39,13 +37,12 @@ def new_family(request):
         form = NewFamilyForm()
     return render(request, 'new_family.html', {'form':form})
 
-def show_up(request, id):
-    family = Family.objects.get(id=id)
-    today = datetime.date.today()
-    today = MeetingDate.objects.get(submission_date=today)
+def show_up(request, name):
+    family = Family.objects.get(name=name)
+    max_date = MeetingDate.objects.latest('submission_date')
     if request.method == 'POST':
         showForm = ShowUpForm(request.POST, instance=family)
-        dateForm = MeetingDateForm(request.POST, instance=today)
+        dateForm = MeetingDateForm(request.POST, instance=max_date)
         if showForm.is_valid() and dateForm.is_valid():
             showForm.save()
             dateForm.save()
@@ -58,16 +55,26 @@ def show_up(request, id):
     return render(request, 'show_up.html', context)
 
 def intermediate_record(request):
-    families = Family.objects.all()
-    for family in families:
-        # reset all family attendence number to 0 in family table
-        Family.objects.filter(name=family.name).update(nAdults=0)
-        Family.objects.filter(name=family.name).update(nUnder12=0)
-        Family.objects.filter(name=family.name).update(nAbove12=0)
+    currentRecord = IntermediateRecord.objects.all()
+    context = {}
+    context['currentRecord'] = currentRecord
     if request.method == 'POST':
         with connection.cursor() as cursor:
             res = cursor.execute(sql_s1+sql_s2+sql_s3+sql_s4+sql_s5)
-    currentDay = IntermediateRecord.objects.all()
+        return redirect('intermediate_record.html')
+    else:
+        return render(request, 'intermediate_record.html', context)
+
+def reset_record(request):
+    currentRecord = IntermediateRecord.objects.all()
     context = {}
-    context['currentDay'] = currentDay
-    return render(request, 'intermediate_record.html', context)
+    context['currentRecord'] = currentRecord
+    if request.method == 'POST':
+        for family in currentRecord:
+            # reset all family attendence number to 0 in family table
+            IntermediateRecord.objects.filter(name=family.name).update(nAdults=0)
+            IntermediateRecord.objects.filter(name=family.name).update(nUnder12=0)
+            IntermediateRecord.objects.filter(name=family.name).update(nAbove12=0)
+        return redirect('reset_record.html')
+    else:
+        return render(request, 'reset_record.html', context)
--- attendence/urls.py
+++ attendence/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^new_family/', views.new_family, name='new_family'),
-    url(r'^show_up/(\d+)/', views.show_up, name='show_up'),
+    url(r'^show_up/(\w)/', views.show_up, name='show_up'),
     url(r'^intermediate_record/', views.intermediate_record, name='intermediate_record'),
+    url(r'^reset_record/', views.reset_record, name='reset_record'),
 ]
